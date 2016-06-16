26728395f0bc0e118000b2863bd0c6751e57e474
Abhishek Juneja <abhishek.juneja145@gmail.com>
2016-06-16 20:12:52 +0530

    added grapelli

--- aurora/urls.py
+++ aurora/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('learn.urls')),
 ]
