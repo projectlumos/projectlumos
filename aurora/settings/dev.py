1a2e0c3b5c6929670fa5e13e7e64a89aa686854d
Abhishek Juneja <abhishek.juneja145@gmail.com>
2016-06-21 12:42:07 +0530

    switched to mysql db

--- aurora/settings/dev.py
+++ aurora/settings/dev.py
@@ -1,13 +1,14 @@
 from .base import *
 
 DEBUG = True
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.mysql', 
         'NAME': get_env_variable('DATABASE_NAME'),
         'USER': get_env_variable('DATABASE_USER'),
         'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
-        'HOST': '',
-        'PORT': '',
-    }
+        'HOST': '', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '', # Set to empty string for default.
+       }
 }
