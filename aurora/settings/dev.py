5df6474873748c98a5f56fa837b121baa651849f
Abhishek Juneja <abhishek.juneja145@gmail.com>
2016-06-21 13:37:41 +0530

    changing the host setting to accomodate PA

--- aurora/settings/dev.py
+++ aurora/settings/dev.py
@@ -8,7 +8,7 @@ DATABASES = {
         'NAME': get_env_variable('DATABASE_NAME'),
         'USER': get_env_variable('DATABASE_USER'),
         'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
-        'HOST': '', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': get_env_variable('DATABASE_HOST'), # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '', # Set to empty string for default.
        }
 }
