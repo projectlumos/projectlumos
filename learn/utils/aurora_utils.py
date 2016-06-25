6d37834c24300f51b75c52502bad945782365f5b
Abhishek Juneja <abhishek.juneja145@gmail.com>
2016-06-25 19:23:53 +0530

    basic layout operational

--- learn/utils/aurora_utils.py
+++ learn/utils/aurora_utils.py
@@ -38,6 +38,24 @@ def get_tech_and_slugs():
             return_data[technology.name] = technology.slug
     return return_data
 
+def get_related_technologies(resource):
+    """
+    returns related technologies for a resource
+    used in the related technology section of the resource page
+    """
+    required_resource = Resources.objects.get(id=resource.id)
+    related_technologies = {tech.name: tech.slug for tech in required_resource.technology.all()}
+    return related_technologies
+
+def get_related_domains(resource):
+    """
+    returns related domains and slugs for a resource
+    used in the related domains sections of the resource layout page
+    """
+    required_resource = Resources.objects.get(id=resource.id)
+    related_domains = {domain.name: domain.slug for domain in required_resource.domain.all()}
+    return related_domains
+
 def resource_to_data(resource):
     """
     Converts a resource queryset into essential fields only
@@ -50,13 +68,17 @@ def resource_to_data(resource):
     return_resource['diff_sort'] = resource.diff_sort
     return_resource['media_type'] = resource.media_type
     return_resource['slug'] = resource.slug
+
+    return_resource['related_technologies'] = get_related_technologies(resource)
+    return_resource['related_domains'] = get_related_domains(resource)
+
     return return_resource
 
 def get_resources_grouped_by_tech(domain):
     """
     Given a domain slug, this function will return
     all the resources for that domain group by technologies
-    i/p: 'python-web-dev'
+    i/p: 'android-dev'
     o/p: 
     """
     return_data = {}
@@ -66,23 +88,26 @@ def get_resources_grouped_by_tech(domain):
 
         for technology in technologies_in_resource:
             tech_name = technology.name
-            curr_tech_resources = return_data.get(tech_name, None)
+            curr_tech_data = return_data.get(tech_name, None)
 
-            if not curr_tech_resources:
-                curr_tech_resources = [resource_to_data(resource)]
+            if not curr_tech_data:
+                curr_tech_data = {}
+                # curr_tech_data['related_domains'] = domains_for_tech(tech_name)
+                # figure out the related domains using smart querying
+                curr_tech_data['resources'] = [resource_to_data(resource)]
+                curr_tech_data['desc'] = technology.desc
             else:
                 print 'adding to an existing tech'
-                curr_tech_resources.append(resource_to_data(resource))
+                curr_tech_data.resources.append(resource_to_data(resource))
 
-            return_data[tech_name] = curr_tech_resources    
+            return_data[tech_name] = curr_tech_data
     return return_data
 
 def get_resources_grouped_by_domain(tech):
     """
     Given a tech slug, this function will return
     all the resources for that tech grouped by domains
-    i/p: 'python-web-dev'
-    o/p: 
+    i/p: 'python'
     """
     return_data = {}
     all_resources = Resources.objects.filter(technology__slug=tech)
@@ -91,14 +116,18 @@ def get_resources_grouped_by_domain(tech):
 
         for domain in domains_in_resource:
             domain_name = domain.name
-            curr_domain_resources = return_data.get(domain_name, None)
-            if not curr_domain_resources:
-                curr_domain_resources = [resource_to_data(resource)]
-            else:
-                print 'adding to an existing domain'
-                curr_domain_resources.append(resource_to_data(resource))
+            curr_domain_data = return_data.get(domain_name, None)
 
-            return_data[domain_name] = curr_domain_resources    
+            if not curr_domain_data:
+                curr_domain_data = {}
+                # curr_domain_data['related_technologies'] = techs_for_domain(domain_name)
+                # figure out the related techs using smart querying
+                curr_domain_data['resources'] = [resource_to_data(resource)]
+                curr_domain_data['desc'] = domain.desc
+            else:
+                curr_domain_data.resources.append(resource_to_data(resource))
+
+            return_data[domain_name] = curr_domain_data
     return return_data
 
 def get_info_by_slug(slug):
--- learn/views.py
+++ learn/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 import json
 from pprint import pprint
 
-#ideal-guacamole imports
+#aurora imports
+from learn.models import Domain, Technology
 from learn.utils.aurora_utils import get_all_domains, get_all_technologies, get_resources_grouped_by_tech, get_resources_grouped_by_domain, get_domains_and_slugs, get_tech_and_slugs
 # Create your views here.
 
@@ -15,24 +16,35 @@ def hello(request):
 
 def get_domain_resources_view(request, domain_slug):
     print domain_slug
-    return_data = None
+    return_data = {}
     try:
-        return_data = get_resources_grouped_by_tech(domain=domain_slug)
+        slug_data = Domain.objects.get(slug=domain_slug)
+        return_data['slug_name'] = slug_data.name
+        return_data['slug_desc'] = slug_data.desc
+        return_data['all_resources'] = get_resources_grouped_by_tech(domain=domain_slug)
+        return_data['mode'] = 'domain'
+        pprint(return_data)
     except Exception, e:
         print e
-        pass
+        return HttpResponse(json.dumps("500 error"))
     pprint(return_data)
-    return HttpResponse(return_data)
+    return render(request, 'resource_layout.html', {'return_data': return_data})
 
 def get_tech_resources_view(request, tech_slug):
-    return_data = None
+    print tech_slug
+    return_data = {}
     try:
-        return_data = get_resources_grouped_by_domain(tech=tech_slug)
+        slug_data = Technology.objects.get(slug=tech_slug)
+        return_data['slug_name'] = slug_data.name
+        return_data['slug_desc'] = slug_data.desc
+        return_data['all_resources'] = get_resources_grouped_by_domain(tech=tech_slug)
+        return_data['mode'] = 'tech'
+        pprint(return_data)
     except Exception, e:
         print e
-        pass
+        return HttpResponse(json.dumps("500 error"))
     pprint(return_data)
-    return HttpResponse(return_data)
+    return render(request, 'resource_layout.html', {'return_data': return_data})
 
 def get_label_data_view(request):
     pass
